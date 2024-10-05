2b80a1373d86f83f36cc9f0d2a8cfd23f23f683c
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-10-05 19:35:32 +0200

    Test

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,3 +3,115 @@
 //
 
 #include "Matrix.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <vector>
+using namespace std;
+Matrix Create_Matrix(int row, int col) {
+        return Matrix(row, col);
+}
+
+Matrix operator+(const Matrix &lhs, const Matrix &rhs) {
+        Matrix lhs1(lhs);
+        return lhs1 += rhs;
+}
+
+Matrix operator-(const Matrix &lhs, const Matrix &rhs) {
+        Matrix lhs1(lhs);
+        return lhs1 -= rhs;
+}
+
+Matrix operator*(const Matrix &lhs, const Matrix &rhs) {
+        Matrix lhs1(lhs);
+        return lhs1 *= rhs;
+}
+
+bool operator==(const Matrix &lhs, const Matrix &rhs) {
+        if (lhs.row != rhs.row || lhs.col != rhs.col) {return false;}
+        for (int i = 0; i < lhs.row; i++) {
+                for (int j = 0; j < lhs.col; j++) {
+                        if (lhs.data[i][j] != rhs.data[i][j]) {return false;}
+                }
+        }
+        return true;
+}
+
+bool operator!=(const Matrix &lhs, const Matrix &rhs) {
+        return !(lhs == rhs);
+}
+
+/**
+ * TODO
+ * @param rhs
+ * @return
+ */
+Matrix Matrix::operator*=(const Matrix &rhs) {
+        return *this;
+}
+
+/**
+ * TODO
+ */
+Matrix::~Matrix() = default;
+
+Matrix::Matrix(int row, const int col) {
+        this->row = row;
+        this->col = col;
+        vector<vector<double>> vec(row, vector<double> (col, 0));
+        this->data = vec;
+
+}
+
+
+Matrix Matrix::operator+=(const Matrix &rhs) {
+        if(!this->EqualSize(rhs)) {
+                cout<<"Error in Matrix::operator+="<<endl;
+
+        }
+        else {
+                for(int i = 0; i < this->row; i++) {
+                        for(int j = 0; j < this->col; j++) {
+                                this->data[i][j] = this->data[i][j] + rhs.data[i][j];
+                        }
+                }
+        }
+        return *this;
+}
+
+Matrix Matrix::operator-=(const Matrix &rhs) {
+        if(!this->EqualSize(rhs)) {
+                cout<<"Error in Matrix::operator+="<<endl;
+
+        }
+        else {
+                for(int i = 0; i < this->row; i++) {
+                        for(int j = 0; j < this->col; j++) {
+                                this->data[i][j] = this->data[i][j] - rhs.data[i][j];
+                        }
+                }
+        }
+        return *this;
+}
+
+Matrix::Matrix(const Matrix &rhs) {
+        this->row = rhs.row;
+        this->col = rhs.col;
+        vector<vector<double>> vec(row, vector<double> (col, 0));
+        this->data = vec;
+        for(int i = 0; i < this->row; i++) {
+                for(int j = 0; j < this->col; j++) {
+                        this->data[i][j] = rhs.data[i][j];
+                }
+        }
+        }
+
+void Matrix::GetSize(int &row, int &col) const {
+        row = this->row;
+        col = this->col;
+}
+bool Matrix::EqualSize(const Matrix &other) const {
+        return this->row == other.row && this->col == other.col;
+}
+
--- Matrix.h
+++ Matrix.h
@@ -4,8 +4,8 @@
 
 #ifndef MATRIX_H
 #define MATRIX_H
-
-
+#include <vector>
+using namespace std;
 /** Class Matrix is a matrix of type double in 2 dimension
 *
 *
@@ -14,16 +14,44 @@
 class Matrix {
 // friend
 friend Matrix Create_Matrix(int row, int col);
-public:
+//operator
+        friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
+        friend Matrix operator-(const Matrix& lhs, const Matrix& rhs);
+        friend Matrix operator*(const Matrix& lhs, const Matrix& rhs);
+        friend bool operator==(const Matrix& lhs, const Matrix& rhs);
+        friend bool operator!=(const Matrix& lhs, const Matrix& rhs);
 
-        Matrix(int row, int col);
+public:
+        Matrix operator+=(const Matrix& rhs);
+        Matrix operator-=(const Matrix& rhs);
+        Matrix operator*=(const Matrix& rhs);
+        Matrix(const Matrix& rhs);
+        ~Matrix();
+
+        //methode
+        /**
+         *@brief
+        * @param row
+        * @param col
+*/
+        void GetSize(int& row, int& col) const;
+
+
+
 
 private:
-int row;
-int col;
-double data[][];
+        Matrix(int row, int col);
+int row{};
+int col{};
+        // row[col]
+vector<vector<double>> data;
+
+        //methode interne
+        bool EqualSize(const Matrix& other) const;
 };
-
-
+/**
+*
+*/
+Matrix Create_Matrix(int row, int col);
 
 #endif //MATRIX_H
