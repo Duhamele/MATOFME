2d6ef7cf9bd556e59c2e8c7a98b48e29bf3753ac
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-10-18 21:31:51 +0200

    petit vendredi soire operator +

--- Matrix_Gentrice.h
+++ Matrix_Gentrice.h
@@ -5,11 +5,26 @@ template<typename T>
 using function_gen= T (int row,int col);
 
 template<typename Type>class Matrix {
+        friend Matrix<Type> operator+(Matrix<Type> const &m1,Matrix<Type> const &m2);
+
+
+
+
 protected:
         int rows;
         int cols;
         vector<vector<Type> > matrix;
         public:
+        Matrix(Matrix<Type> const &m1) {
+                rows = m1.rows;
+                cols = m1.cols;
+                matrix= vector<vector<Type> >(rows,vector<Type>(cols));
+                for(int i=0;i<rows;i++) {
+                        for(int j=0;j<cols;j++) {
+                                matrix[i][j] = m1.matrix[i][j];
+                        }
+                }
+        }
         Matrix(int rows, int cols,Type valeur_base) {
                 if (rows < 1 || cols < 1) {
                         throw std::invalid_argument("Invalid matrix size");
@@ -52,7 +67,22 @@ protected:
                 }
         }
 
+
 };
+template<typename Type>
+Matrix<Type> operator+(Matrix<Type> const &m1,Matrix<Type> const &m2) {
+        if (m1.rows != m2.rows || m1.cols != m2.cols) {
+                throw std::invalid_argument("Invalid matrix size");
+        }
+        Matrix<Type> result(m1.rows,m1.cols,Type(0));
+        for (int i = 0; i < m1.rows; i++) {
+                for (int j = 0; j < m1.cols; j++) {
+                        result.matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
+                }
+        }
+        return result;
+
+}
 
 
 #endif
